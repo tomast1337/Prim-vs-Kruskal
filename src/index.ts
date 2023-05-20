a60e1f0fb97db1b733d04ce734f7ab89ac6b40dd
tomast1337 <vycasnicolas@gmail.com>
2023-05-20 17:47:37 -0300

    Improving the -file option

--- src/index.ts
+++ src/index.ts
@@ -14,17 +14,21 @@ import { example as examplePrimAlgorithm } from "./entities/prim-algorithm";
 
 // get run parameters
 
-const args = process.argv.slice(2);
+const args = process.argv.slice(2); // remove first two arguments (node and index.js)
+
+// get -seed argument
 
 //if has -debug flag, run example code
 if (args.includes("-debug")) {
   exampleEdge();
   exampleNode();
   exampleGraph();
-  exampleGraphDrawer();
-  exampleGraphLoader();
+
   exampleKruskalAlgorithm();
   examplePrimAlgorithm();
+
+  exampleGraphDrawer();
+  exampleGraphLoader();
   // exit process
   process.exit(0);
 }
@@ -34,9 +38,21 @@ if (args.includes("-file")) {
   const filePath = args[args.indexOf("-file") + 1];
   const graphLoader = new GraphLoader();
   const loadedGraph = graphLoader.loadFromFile(filePath);
+  console.log(`Successfully loaded graph from file ${filePath}`);
+
+  const seedIndex = args.indexOf("-seed");
+  let seed = "undefined";
+  if (seedIndex !== -1) {
+    seed = args[seedIndex + 1];
+  }
+
   if (loadedGraph) {
-    const graphDrawer = new GraphDrawer<string>();
-    graphDrawer.drawGraph(loadedGraph).save("graph.svg");
+    const graphDrawer = new GraphDrawer<string>(1000, 1000, seed);
+    const path = `${filePath.split("/")[filePath.split("/").length - 1]}-${
+      new Date().toISOString().split("T")[0]
+    }.svg`;
+    graphDrawer.drawGraph(loadedGraph).save(path);
+    console.log(`Successfully saved graph to file ${path}`);
   }
   process.exit(0);
 }
